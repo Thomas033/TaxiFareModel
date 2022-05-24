d5cf35f6af4ddd8236807f9dd4aceda2a75f955d
thomas lemoine <tomlemoine@msn.com>
2022-05-24 14:46:31 +0200

    added ML FLOW

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -4,6 +4,11 @@ from sklearn.compose import ColumnTransformer
 from sklearn.linear_model import LinearRegression
 from TaxiFareModel.encoders import TimeFeaturesEncoder, DistanceTransformer
 from TaxiFareModel.utils import compute_rmse
+import mlflow
+from mlflow.tracking import MlflowClient
+from memoized_property import memoized_property
+
+
 
 class Trainer():
     def __init__(self, X, y):
@@ -14,6 +19,8 @@ class Trainer():
         self.pipe = None
         self.X = X
         self.y = y
+        self.MLFLOW_URI = "https://mlflow.lewagon.ai/"
+        self.experiment_name = "[FR] [Nantes] [ThomasL33] Taxi_Fare + V1"
 
     # def set_pipeline(self):
     #     """defines the pipeline as a class attribute"""
@@ -53,6 +60,31 @@ class Trainer():
         y_pred = self.pipe.predict(X_test)
         rmse = compute_rmse(y_pred, y_test)
         return rmse
+
+    @memoized_property
+    def mlflow_client(self):
+        mlflow.set_tracking_uri(self.MLFLOW_URI)
+        return MlflowClient()
+
+    @memoized_property
+    def mlflow_experiment_id(self):
+        try:
+            return self.mlflow_client.create_experiment(self.experiment_name)
+        except BaseException:
+            return self.mlflow_client.get_experiment_by_name(self.experiment_name).experiment_id
+
+    @memoized_property
+    def mlflow_run(self):
+        return self.mlflow_client.create_run(self.mlflow_experiment_id)
+
+    def mlflow_log_param(self, key, value):
+        self.mlflow_client.log_param(self.mlflow_run.info.run_id, key, value)
+
+    def mlflow_log_metric(self, key, value):
+        self.mlflow_client.log_metric(self.mlflow_run.info.run_id, key, value)
+
+
+EXPERIMENT_NAME = "[country code] [city] [login] model name + version"
 
 
 if __name__ == "__main__":
